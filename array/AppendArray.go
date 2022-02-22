de7118f2c3adae10b7efecc7020f38428e3cbaec
xx <>
2022-02-22 22:05:21 +0700

    array

--- array/AppendArray.go
+++ array/AppendArray.go
@@ -3,17 +3,16 @@ package array
 import "fmt"
 
 func AppendArray() {
-	var sl []int
+	var sl = make([]int, 4)
 
-	sl = make([]int, 4)
-
-	sl[0] = 5
 	fmt.Println("------------------- AppendArray ----------------------")
-
+	sl[0] = 5
+	sl[2] = 60
 	sl = append(sl, 20)
+
 	fmt.Printf("%v \n", sl[0])
 	fmt.Printf("%v \n", sl[1])
 	fmt.Printf("%v \n", sl[2])
 	fmt.Printf("%v \n", sl[3])
-	fmt.Printf("%v \n", sl[4])
+	fmt.Printf("%v \n", sl)
 }
--- array/Slice.go
+++ array/Slice.go
@@ -3,12 +3,11 @@ package array
 import "fmt"
 
 func Slice() {
-	var sl []int
-
-	sl = make([]int, 4)
+	var sl = make([]int, 4)
 	fmt.Println("------------------- Slice ----------------------")
 
 	sl[0] = 5
+	sl[2] = 9
 	fmt.Printf("%v \n", sl[0])
 	fmt.Printf("%v \n", sl[1])
 	fmt.Printf("%v \n", sl[2])
